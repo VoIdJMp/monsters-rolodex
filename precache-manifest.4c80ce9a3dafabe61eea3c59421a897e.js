2b874d4cd89739ab82e0c92fda91a2022e5c77f4
Azizjon Muminov <azizjon@Azizjons-MacBook-Pro.local>
2020-02-21 23:28:42 +0500

    Updates

---
+++ precache-manifest.4c80ce9a3dafabe61eea3c59421a897e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2516065d8b2f702b58ee98d42146ab0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d4d0b2d5de37178d97b2",
+    "url": "/monsters-rolodex/static/css/main.0dd0f45c.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4d0b2d5de37178d97b2",
+    "url": "/monsters-rolodex/static/js/main.98831e4d.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
